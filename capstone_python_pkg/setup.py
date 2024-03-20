cf691a66f83f2efc234b602b7ac4055984268077
xogk991210 <xogk991210@gmail.com>
2024-03-20 18:56:50 +0900

    캡스톤 디렉토리에 설정 변경

--- capstone_python_pkg/setup.py
+++ capstone_python_pkg/setup.py
@@ -1,25 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'capstone_python_pkg'
+package_name = "capstone_python_pkg"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='rlaxgk',
-    maintainer_email='xogk991210@gmail.com',
-    description='Capstone',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="rlaxgk",
+    maintainer_email="xogk991210@gmail.com",
+    description="Capstone",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "lidar_filters = capstone_python_pkg.2DLiadr_ObjectDetection_PythonCode:main",
         ],
     },
 )
